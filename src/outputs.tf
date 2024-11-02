d2a26a78b0261b0de8fb8b3a0d3a84f493811345
Igor Rodionov <goruha@gmail.com>
2024-11-02 19:22:22 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,14 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "id" {
+  description = "The ID of the WAF WebACL."
+  value       = module.aws_waf.id
+}
+
+output "arn" {
+  description = "The ARN of the WAF WebACL."
+  value       = module.aws_waf.arn
+}
+
+output "logging_config_id" {
+  description = "The ARN of the WAFv2 Web ACL logging configuration."
+  value       = module.aws_waf.logging_config_id
 }
